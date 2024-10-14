f5e590919aea40f62362784d83e34f45beaaa170
jumagu <jmgg.gutierrez@gmail.com>
2024-10-14 15:18:03 -0500

    fix: validate the page number to ensure that it will always be a valid number.

--- src/modules/shop/views/HomeView.vue
+++ src/modules/shop/views/HomeView.vue
@@ -4,13 +4,15 @@ import { useRoute } from 'vue-router';
 
 import { useQuery, useQueryClient } from '@tanstack/vue-query';
 
+import TabMenu from '../components/TabMenu.vue';
 import { getProducts } from '@/modules/products/actions';
 import ProductCard from '@/modules/products/components/ProductCard.vue';
 import ProductGrid from '@/modules/products/components/ProductGrid.vue';
 import MyPagination from '@/modules/common/components/ui/MyPagination.vue';
+import { getPageNumber } from '@/modules/common/utils/get-page-number.util';
 
 const route = useRoute();
-const page = ref(Number(route.query.page ?? 1));
+const page = ref(getPageNumber(route.query.page as string));
 const queryClient = useQueryClient();
 
 const { data: products } = useQuery({
@@ -21,7 +23,7 @@ const { data: products } = useQuery({
 watch(
   () => route.query.page,
   (newPage) => {
-    page.value = Number(newPage ?? 1);
+    page.value = getPageNumber(newPage as string);
     window.scrollTo({ top: 0, behavior: 'smooth' });
   },
 );
@@ -30,7 +32,16 @@ watchEffect(() => {
   queryClient.prefetchQuery({
     queryKey: ['products', { page: page.value + 1 }],
     queryFn: () => getProducts(page.value + 1),
+    staleTime: 1000 * 60,
   });
+
+  if (page.value > 1) {
+    queryClient.prefetchQuery({
+      queryKey: ['products', { page: page.value - 1 }],
+      queryFn: () => getProducts(page.value - 1),
+      staleTime: 1000 * 60,
+    });
+  }
 });
 </script>
 
@@ -39,95 +50,15 @@ watchEffect(() => {
     <h1 class="text-center text-2xl font-bold text-gray-800">All Products</h1>
   </div>
 
-  <!-- Tab Menu -->
-  <div
-    class="flex flex-wrap items-center overflow-x-auto overflow-y-hidden py-10 justify-center bg-white text-gray-800"
-  >
-    <a
-      rel="noopener noreferrer"
-      href="#"
-      class="flex items-center flex-shrink-0 px-5 py-3 space-x-2text-gray-600"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="w-4 h-4"
-      >
-        <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
-      </svg>
-      <span>Architecto</span>
-    </a>
-    <a
-      rel="noopener noreferrer"
-      href="#"
-      class="flex items-center flex-shrink-0 px-5 py-3 space-x-2 rounded-t-lg text-gray-900"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="w-4 h-4"
-      >
-        <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
-        <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
-      </svg>
-      <span>Corrupti</span>
-    </a>
-    <a
-      rel="noopener noreferrer"
-      href="#"
-      class="flex items-center flex-shrink-0 px-5 py-3 space-x-2 text-gray-600"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="w-4 h-4"
-      >
-        <polygon
-          points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
-        ></polygon>
-      </svg>
-      <span>Excepturi</span>
-    </a>
-    <a
-      rel="noopener noreferrer"
-      href="#"
-      class="flex items-center flex-shrink-0 px-5 py-3 space-x-2 text-gray-600"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="w-4 h-4"
-      >
-        <circle cx="12" cy="12" r="10"></circle>
-        <polygon points="16.24 7.76 14.12 14.12 7.76 16.24 9.88 9.88 16.24 7.76"></polygon>
-      </svg>
-      <span>Consectetur</span>
-    </a>
-  </div>
+  <tab-menu />
 
-  <product-grid>
+  <product-grid v-if="products && products.length > 0">
     <product-card v-for="product in products" :product="product" :key="product.id" />
   </product-grid>
+
+  <div v-else class="py-10">
+    <p class="mx-2 text-center text-xl">There are no results for this page.</p>
+  </div>
 
   <my-pagination :page="page" :no-more-data="!!products && products.length < 10" />
 </template>
