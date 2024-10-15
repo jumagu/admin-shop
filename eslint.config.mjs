62e2eaf16657299cd4e7eaf5a6ff87dc3f1f9c36
jumagu <jmgg.gutierrez@gmail.com>
2024-10-14 23:26:51 -0500

    chore: format file and turn off @typescript-eslint/no-explicit-any rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,23 @@
-import pluginVue from 'eslint-plugin-vue'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
-import pluginVitest from '@vitest/eslint-plugin'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import pluginVue from 'eslint-plugin-vue';
+import pluginVitest from '@vitest/eslint-plugin';
+import vueTsEslintConfig from '@vue/eslint-config-typescript';
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
 
 export default [
   {
     name: 'app/files-to-lint',
+    '@typescript-eslint/no-explicit-any': 'off',
     files: ['**/*.{ts,mts,tsx,vue}'],
   },
-
   {
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
-
   ...pluginVue.configs['flat/essential'],
   ...vueTsEslintConfig(),
-  
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
   skipFormatting,
-]
+];
