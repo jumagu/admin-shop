b0a8a7b52e1015930a0a72684253a850a8666f1d
jumagu <jmgg.gutierrez@gmail.com>
2024-10-17 23:08:47 -0500

    refactor: improve error handing

--- src/modules/products/actions/get-products.ts
+++ src/modules/products/actions/get-products.ts
@@ -1,22 +1,23 @@
 import shopApi from '@/apis/shop.api';
+import { getProductImage } from '../helpers';
 import type { Product } from '../interfaces/product.interface';
-import { getProductImage } from './get-product-image';
 
-export const getProducts = async (page: number = 1, limit: number = 10) => {
+export const getProducts = async (page: number = 1, limit: number = 10): Promise<Product[]> => {
   try {
-    const { data } = await shopApi.get<Product[]>('/products', {
+    const res = await shopApi.get<Product[]>('/products', {
       params: {
         limit,
         offset: page * limit,
       },
     });
 
-    return data.map((product) => ({
+    if (res.status !== 200) throw new Error();
+
+    return res.data.map((product) => ({
       ...product,
       images: product.images.map(getProductImage),
     }));
-  } catch (error) {
-    console.log(error);
-    throw new Error('');
+  } catch {
+    throw new Error('An error occurred while fetching products.');
   }
 };
--- src/modules/products/actions/index.ts
+++ src/modules/products/actions/index.ts
@@ -1,2 +1,2 @@
 export * from './get-products';
-export * from './get-product-image';
+export * from './get-product-by-id';
