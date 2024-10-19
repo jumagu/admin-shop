b1b7ee6c2a25f750a66cb1dec8736b5c05f7582c
jumagu <jmgg.gutierrez@gmail.com>
2024-10-19 14:28:08 -0500

    refactor: add a button to redirect to product creation form

--- src/modules/admin/views/ProductsView.vue
+++ src/modules/admin/views/ProductsView.vue
@@ -17,7 +17,7 @@ const { data: products, error } = useQuery({
 });
 
 watch(error, (error) => {
-  return error?.message && toast.error(error?.message);
+  return error?.message && toast.error(error.message);
 });
 
 watchEffect(() => {
@@ -38,35 +38,49 @@ watchEffect(() => {
 </script>
 
 <template>
-  <div class="w-full shadow overflow-auto">
-    <table class="w-full bg-white">
-      <thead class="bg-gray-800 text-white">
-        <tr>
-          <th class="text-left py-3 px-4 uppercase font-semibold text-sm">Image</th>
-          <th class="text-left py-3 px-4 uppercase font-semibold text-sm">Title</th>
-          <th class="text-left py-3 px-4 uppercase font-semibold text-sm">Price</th>
-          <th class="text-left py-3 px-4 uppercase font-semibold text-sm">Sizes</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="(product, index) in products"
-          :class="{ 'bg-gray-100': index % 2 === 0 }"
-          :key="product.id"
-        >
-          <td class="py-3 px-4">
-            <img class="w-11 h-11" :src="product.images[0]" :alt="product.title" />
-          </td>
-          <td class="py-3 px-4">
-            <router-link :to="`product/${product.id}`" class="hover:underline hover:text-blue-500">
-              {{ product.title }}
-            </router-link>
-          </td>
-          <td class="py-3 px-4">${{ product.price }}</td>
-          <td class="py-3 px-4">{{ product.sizes.join(', ') }}</td>
-        </tr>
-      </tbody>
-    </table>
+  <div class="w-full">
+    <div class="flex mb-2">
+      <button
+        class="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 ml-auto rounded"
+        @click="$router.push('product/new')"
+      >
+        Add new
+      </button>
+    </div>
+
+    <div class="overflow-auto">
+      <table class="w-full bg-white">
+        <thead class="bg-gray-800 text-white">
+          <tr>
+            <th class="min-w-24 text-left py-3 px-4 uppercase font-semibold text-sm">Image</th>
+            <th class="min-w-[420px] text-left py-3 px-4 uppercase font-semibold text-sm">Title</th>
+            <th class="min-w-20 text-left py-3 px-4 uppercase font-semibold text-sm">Price</th>
+            <th class="min-w-52 text-left py-3 px-4 uppercase font-semibold text-sm">Sizes</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(product, index) in products"
+            :class="{ 'bg-gray-100': index % 2 === 0 }"
+            :key="product.id"
+          >
+            <td class="py-3 px-4">
+              <img class="w-11 h-11" :src="product.images[0]" :alt="product.title" />
+            </td>
+            <td class="py-3 px-4">
+              <router-link
+                :to="`product/${product.id}`"
+                class="hover:underline hover:text-blue-500"
+              >
+                {{ product.title }}
+              </router-link>
+            </td>
+            <td class="py-3 px-4">${{ product.price }}</td>
+            <td class="py-3 px-4">{{ product.sizes.join(', ') }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
 
     <my-pagination class="py-8" :page="page" :no-more-data="!!products && products.length < 10" />
   </div>
