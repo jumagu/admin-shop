bd1d9634003f70066a799c1f881089ace77c79de
jumagu <jmgg.gutierrez@gmail.com>
2024-10-19 16:43:27 -0500

    refactor: make the necessary modifications to the form to make submission work for the creation or update of a product

--- src/modules/admin/components/product/ProductForm.ts
+++ src/modules/admin/components/product/ProductForm.ts
@@ -1,8 +1,12 @@
-import { defineComponent, type PropType } from 'vue';
+import { watch, defineComponent, type PropType, ref } from 'vue';
 import { useForm, useFieldArray } from 'vee-validate';
+import { useMutation } from '@tanstack/vue-query';
+import { useToast } from 'vue-toastification';
+import { useRouter } from 'vue-router';
 
 import * as yup from 'yup';
 
+import { createUpdateProduct } from '@/modules/products/actions';
 import CustomInput from '@/modules/common/components/ui/CustomInput.vue';
 import CustomSelect from '@/modules/common/components/ui/CustomSelect.vue';
 import CustomTextarea from '@/modules/common/components/ui/CustomTextarea.vue';
@@ -10,25 +14,37 @@ import type { Product } from '../../../products/interfaces/product.interface';
 
 const validationSchema = yup.object({
   title: yup.string().trim().min(5).required(),
-  slug: yup.string().required(),
-  description: yup.string().required(),
-  price: yup.number().required(),
-  stock: yup.number().required().min(1),
-  gender: yup.string().required().oneOf(['men', 'women', 'kid']),
+  slug: yup.string().trim().min(5).required(),
+  description: yup.string().trim().min(20).required(),
+  price: yup.number().min(0).required(),
+  stock: yup.number().min(1).required(),
+  gender: yup.string().required().oneOf(['men', 'women', 'kid', 'unisex']),
 });
 
 export default defineComponent({
   props: {
-    product: {
-      type: Object as PropType<Product>,
-      required: true,
-    },
+    product: Object as PropType<Product | null>,
   },
 
   setup(props) {
-    const { defineField, errors, handleSubmit } = useForm({
+    const router = useRouter();
+    const toast = useToast();
+
+    const {
+      mutate,
+      isPending,
+      isSuccess,
+      isError,
+      error,
+      data: mutatedProduct,
+    } = useMutation({
+      mutationFn: createUpdateProduct,
+    });
+
+    // Form refs
+    const { defineField, errors, handleSubmit, resetForm } = useForm({
       validationSchema,
-      initialValues: props.product,
+      initialValues: props.product ? props.product : { images: [] },
     });
 
     const [title, titleAttrs] = defineField('title');
@@ -40,6 +56,7 @@ export default defineComponent({
 
     const { fields: sizes, push: pushSize, remove: removeSize } = useFieldArray<string>('sizes');
     const { fields: images } = useFieldArray<string>('images');
+    const imageFiles = ref<File[]>([]);
 
     const hasSize = (size: string) => sizes.value.map((s) => s.value).includes(size);
 
@@ -55,7 +72,56 @@ export default defineComponent({
     };
 
     const submitHandler = handleSubmit((values) => {
-      console.log(values);
+      const formValues = {
+        ...values,
+        images: [...values.images, ...imageFiles.value],
+      };
+      mutate(formValues);
+    });
+
+    const fileChangeHandler = (event: Event) => {
+      const fileList = (event.target as HTMLInputElement).files;
+
+      if (!fileList || !fileList.length) return;
+
+      for (const imgFile of fileList) {
+        imageFiles.value.push(imgFile);
+      }
+    };
+
+    const tempImgUrl = (imgFile: File) => {
+      return URL.createObjectURL(imgFile);
+    };
+
+    // Listening product prop changes
+    watch(
+      () => props.product,
+      (product) => {
+        if (!product) return;
+        resetForm({ values: product });
+      },
+      { deep: true, immediate: true },
+    );
+
+    // Listening success submission
+    watch(isSuccess, (value) => {
+      if (!value) return;
+
+      if (mutatedProduct.value?.msg === 'created') {
+        router.replace({ name: 'admin-product', params: { id: mutatedProduct.value?.product.id } });
+        toast.success('Product successfully created.');
+      } else {
+        resetForm({ values: mutatedProduct.value?.product });
+        toast.success('Product successfully updated.');
+      }
+
+      imageFiles.value = [];
+    });
+
+    // Listening error submission
+    watch(isError, (value) => {
+      if (!value) return;
+      return error.value?.message && toast.error(error.value.message);
     });
 
     return {
@@ -75,9 +141,13 @@ export default defineComponent({
       sizes,
       hasSize,
       images,
+      imageFiles,
       allSizes: ['XS', 'S', 'M', 'L', 'XL', 'XXL'] as const,
+      isPending,
       submitHandler,
       selectSizeHandler,
+      fileChangeHandler,
+      tempImgUrl,
     };
   },
   components: {
--- src/modules/admin/components/product/ProductForm.vue
+++ src/modules/admin/components/product/ProductForm.vue
@@ -22,7 +22,7 @@
       type="number"
       class="mb-4 flex-1"
       label="Price"
-      v-model="price"
+      v-model.number="price"
       v-bind="priceAttrs"
       :error="errors.price"
     />
@@ -30,7 +30,7 @@
       type="number"
       class="mb-4 flex-1"
       label="Stock"
-      v-model="stock"
+      v-model.number="stock"
       v-bind="stockAttrs"
       :error="errors.stock"
     />
@@ -75,27 +75,50 @@
       <option value="men">Hombre</option>
       <option value="women">Mujer</option>
       <option value="kid">Niño</option>
+      <option value="unisex">Unisex</option>
     </custom-select>
 
     <div class="mb-4 sm:col-span-2">
       <p class="form-label">Images</p>
-      <div class="flex gap-2 overflow-x-auto w-full h-[265px]">
-        <div v-for="image of images" :key="image.value" class="flex-shrink-0">
-          <img :src="image.value" :alt="title" class="w-[250px] h-[250px]" />
+      <div
+        class="flex items-center overflow-x-auto w-full min-h-[250px] bg-gray-100 rounded border-2 border-gray-500 border-dotted"
+      >
+        <div v-if="!images.length && !imageFiles.length" class="mx-auto">
+          <span class="text-gray-500 text-lg font-semibold select-none">Image Preview</span>
         </div>
+        <template v-else>
+          <div v-for="image of images" :key="image.value" class="flex-shrink-0">
+            <img :src="image.value" :alt="title" class="ml-2 size-[234px] object-cover" />
+          </div>
+          <div v-for="imgFile of imageFiles" :key="imgFile.name" class="flex-shrink-0">
+            <img
+              :src="tempImgUrl(imgFile)"
+              :alt="imgFile.name"
+              class="ml-2 size-[234px] object-cover"
+            />
+          </div>
+        </template>
       </div>
       <div class="my-2">
         <label for="image" class="form-label">Upload Images</label>
-        <input multiple type="file" id="image" class="form-control" />
+        <input
+          id="image"
+          type="file"
+          multiple
+          accept="image/*"
+          class="form-control"
+          @change="fileChangeHandler"
+        />
       </div>
     </div>
 
     <div class="sm:col-span-2">
       <button
         type="submit"
-        class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        class="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded disabled:bg-blue-300 disabled:cursor-not-allowed"
+        :disabled="isPending"
       >
-        Save
+        {{ product ? 'Update' : 'Create' }}
       </button>
     </div>
   </form>
