19860a2120d6ce3875dc798e9ec34fbf4c6fcf4f
jumagu <jmgg.gutierrez@gmail.com>
2024-10-19 14:28:53 -0500

    refactor: remove unused elements and fix resposive issues

--- src/modules/admin/layouts/AdminLayout.vue
+++ src/modules/admin/layouts/AdminLayout.vue
@@ -1,199 +1,70 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
 import { SIDE_NAV_ITEMS } from '../config/side-bar.config';
 import { useAuthStore } from '@/modules/auth/stores/auth.store';
+import MenuIcon from '@/modules/common/components/icons/MenuIcon.vue';
+import LogoutIcon from '@/modules/common/components/icons/LogoutIcon.vue';
 
+const router = useRouter();
 const authStore = useAuthStore();
+
+const logoutHandler = () => {
+  authStore.startLogout();
+  router.replace('/');
+};
 </script>
 
 <template>
-  <div class="flex w-screen h-screen text-gray-700">
-    <div class="flex flex-col items-center w-16 pb-4 overflow-auto border-r border-gray-300">
-      <router-link
-        to="/"
-        class="flex items-center justify-center flex-shrink-0 w-full h-16 border-b border-gray-300"
-      >
-        <img class="h-8 w-8" src="../../../assets/logo.svg" alt="Admin Shop Logo" />
-      </router-link>
-      <a
-        class="flex items-center justify-center flex-shrink-0 w-10 h-10 mt-4 rounded hover:bg-gray-300"
-        href="#"
-      >
-        <svg
-          class="w-5 h-5"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-          />
-        </svg>
-      </a>
-      <a
-        class="flex items-center justify-center flex-shrink-0 w-10 h-10 mt-4 rounded hover:bg-gray-300"
-        href="#"
-      >
-        <svg
-          class="w-5 h-5"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-          />
-        </svg>
-      </a>
-      <a
-        class="flex items-center justify-center flex-shrink-0 w-10 h-10 mt-4 rounded hover:bg-gray-300"
-        href="#"
-      >
-        <svg
-          class="w-5 h-5"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
-          />
-        </svg>
-      </a>
-      <a
-        class="flex items-center justify-center flex-shrink-0 w-10 h-10 mt-4 rounded hover:bg-gray-300"
-        href="#"
-      >
-        <svg
-          class="w-5 h-5"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-          />
-        </svg>
-      </a>
-      <a
-        class="flex items-center justify-center flex-shrink-0 w-10 h-10 mt-4 rounded hover:bg-gray-300"
-        href="#"
-      >
-        <svg
-          class="w-5 h-5"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
-          />
-        </svg>
-      </a>
-      <a
-        class="flex items-center justify-center flex-shrink-0 w-10 h-10 mt-auto rounded hover:bg-gray-300"
-        href="#"
-      >
-        <svg
-          class="w-5 h-5"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-      </a>
+  <div class="drawer lg:drawer-open text-gray-700">
+    <input id="admin-drawer" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content">
+      <div class="flex flex-col flex-1">
+        <header class="sticky top-0 flex items-center h-16 px-6 border-b border-gray-300 bg-white">
+          <label
+            for="admin-drawer"
+            class="p-2 mr-1 w-10 h-10 rounded hover:bg-gray-300 cursor-pointer lg:hidden"
+          >
+            <menu-icon class="size-auto" />
+          </label>
+          <button
+            class="flex items-center gap-2 ml-auto p-2 w-auto h-10 rounded hover:bg-gray-200"
+            @click="logoutHandler"
+          >
+            <span class="text-blue-500">{{ authStore.user?.fullName }}</span>
+            <logout-icon class="size-5" />
+          </button>
+        </header>
+
+        <main class="p-6 bg-gray-200">
+          <router-view />
+        </main>
+      </div>
     </div>
 
-    <div class="flex flex-col w-56 border-r border-gray-300">
-      <div class="flex items-center w-full h-16 px-4 border-b border-gray-300">
-        <h1 class="text-xl font-semibold">Admin Shop</h1>
-      </div>
+    <div class="drawer-side z-[2]">
+      <label for="admin-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+      <aside class="h-full w-60 bg-white border-r border-gray-300">
+        <div class="flex items-center gap-2 w-full h-16 px-4 border-b border-gray-300">
+          <router-link to="/">
+            <img class="h-8 w-8" src="../../../assets/logo.svg" alt="Admin Shop Logo" />
+          </router-link>
+          <h1 class="text-xl font-semibold">Admin Shop</h1>
+        </div>
 
-      <div class="flex flex-col flex-grow p-4 overflow-auto">
-        <nav>
+        <nav class="p-4 overflow-auto">
           <ul role="navigation">
             <li v-for="(item, index) in SIDE_NAV_ITEMS" :key="index" role="link">
               <router-link
                 :to="item.href"
-                class="flex items-center flex-shrink-0 h-10 px-2 text-sm font-medium rounded hover:bg-gray-300"
+                active-class="bg-gray-200"
+                class="flex items-center flex-shrink-0 h-10 px-2 text-sm font-medium rounded hover:bg-gray-200"
               >
                 <span class="leading-none">{{ item.text }}</span>
               </router-link>
             </li>
           </ul>
         </nav>
-      </div>
-    </div>
-
-    <div class="flex flex-col flex-grow">
-      <div class="flex items-center flex-shrink-0 h-16 px-8 border-b border-gray-300">
-        <h1 class="text-lg font-medium">{{ authStore.user?.fullName }}</h1>
-        <button
-          class="flex items-center justify-center h-10 px-4 ml-auto text-sm font-medium rounded hover:bg-gray-300"
-        >
-          Action 1
-        </button>
-        <button
-          class="flex items-center justify-center h-10 px-4 ml-2 text-sm font-medium bg-gray-200 rounded hover:bg-gray-300"
-        >
-          Action 2
-        </button>
-        <button class="relative ml-2 text-sm focus:outline-none group">
-          <div class="flex items-center justify-between w-10 h-10 rounded hover:bg-gray-300">
-            <svg
-              class="w-5 h-5 mx-auto"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
-              />
-            </svg>
-          </div>
-          <div
-            class="absolute right-0 flex-col items-start hidden w-40 pb-1 bg-white border border-gray-300 shadow-lg group-focus:flex"
-          >
-            <a class="w-full px-4 py-2 text-left hover:bg-gray-300" href="#">Menu Item 1</a>
-            <a class="w-full px-4 py-2 text-left hover:bg-gray-300" href="#">Menu Item 1</a>
-            <a class="w-full px-4 py-2 text-left hover:bg-gray-300" href="#">Menu Item 1</a>
-          </div>
-        </button>
-      </div>
-
-      <div class="flex-grow p-6 overflow-auto bg-gray-200">
-        <router-view />
-      </div>
+      </aside>
     </div>
   </div>
 </template>
