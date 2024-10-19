3a25ad12a01f06e401753f70e5cd2da2161de54e
jumagu <jmgg.gutierrez@gmail.com>
2024-10-19 16:45:26 -0500

    refactor: remove tanstack query devtools

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { VueQueryDevtools } from '@tanstack/vue-query-devtools';
 import { useAuthStore } from './modules/auth/stores/auth.store';
 import FullScreenLoader from './modules/common/components/ui/FullScreenLoader.vue';
 
@@ -9,5 +8,4 @@ const authStore = useAuthStore();
 <template>
   <full-screen-loader v-if="authStore.isChecking" />
   <router-view v-else />
-  <vue-query-devtools />
 </template>
