cdf8473f53405941d6605a7e6e3ac98e5a4e1813
jumagu <jmgg.gutierrez@gmail.com>
2024-10-20 11:53:05 -0500

    refactor: add beforeEach guard and not found page route

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import { authRoutes } from '@/modules/auth/routes';
 import { adminRoutes } from '@/modules/admin/routes';
 import ShopLayout from '@/modules/shop/layouts/ShopLayout.vue';
 import { useAuthStore } from '@/modules/auth/stores/auth.store';
+import NotFoundPage from '@/modules/common/pages/NotFoundPage.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,11 +12,6 @@ const router = createRouter({
     {
       path: '/',
       name: 'shop',
-      beforeEnter: async (_, __, next) => {
-        const authStore = useAuthStore();
-        if (authStore.isChecking) await authStore.startCheckAuth();
-        next();
-      },
       component: ShopLayout,
       children: [
         {
@@ -27,7 +23,18 @@ const router = createRouter({
     },
     authRoutes,
     adminRoutes,
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: NotFoundPage,
+    },
   ],
+});
+
+router.beforeEach(async (_, __, next) => {
+  const authStore = useAuthStore();
+  if (authStore.isChecking) await authStore.startCheckAuth();
+  next();
 });
 
 export default router;
